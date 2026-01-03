a1527eb8b6689b47796e2e90d018456cbb83510f
Lucas Fry <frenchfrysays.alt@gmail.com>
2026-01-02 20:20:52 -0600

    Added actual code

--- src/main.cpp
+++ src/main.cpp
@@ -1,33 +1,34 @@
 #include <Geode/Geode.hpp>
+#include <Geode/modify/PlayerObject.hpp>
 
 using namespace geode::prelude;
 
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	bool init() {
-		if (!MenuLayer::init()) {
-			return false;
-		}
+class $modify(PlayerObject) {
+	void update(float dt) {
+		// Call the original update function so the game doesn't break
+		PlayerObject::update(dt);
 
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+		// 1. Get the current time from the global director
+        auto time = cocos2d::CCDirector::sharedDirector()->getTotalFrames() / 60.0f;
+        
+        // 2. Calculate the Rainbow color
+        // Speed: multiply 'time' by a higher number for faster rainbow
+        float speed = 2.0f;
+        float hue = fmod(time * speed * 60.0f, 360.0f);
+        
+        // 3. Convert HSV to a color Geometry Dash understands (ccColor3B)
+        // We use a Geode helper for this!
+        cocos2d::ccColor3B rainbowColor = geode::utils::cocos::colorFromHSV(hue, 1.0f, 1.0f);
 
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			menu_selector(MyMenuLayer::onMyButton)
-		);
+        // 4. Apply the color to the player
+        // This covers both Primary and Secondary colors
+        this->setColor(rainbowColor);
+        this->setSecondColor(rainbowColor);
 
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
-
-		myButton->setID("my-button"_spr);
-
-		menu->updateLayout();
-
-		return true;
+        // Optional: If you want the glow to be rainbow too
+        if (this->m_hasGlow) {
+            this->m_glowColor = rainbowColor;
+            this->updateGlowColor();
+        }
 	}
-
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
-};
+}
